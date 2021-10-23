c6bcb783a9960358674fc31312014b6fb1754439
daleclack <daleclack1@foxmail.com>
2021-10-23 15:58:35 +0800

    Add menu

--- Gtk4/src/MainWin.cpp
+++ Gtk4/src/MainWin.cpp
@@ -3,8 +3,9 @@
 struct _MainWin{
     GtkApplicationWindow parent;
     GtkWidget * background;
-    GtkWidget * draw_area;
     GtkWidget * overlay;
+    GtkGesture * gesture;
+    GtkWidget * popover;
     int width,height;
 };
 
@@ -14,11 +15,25 @@ static void change_background(GtkWindow * dialog,int response,MainWin *win){
     gtk_window_destroy(dialog);
 }
 
-static void background_dialog(GSimpleAction * action, GVariant * parmeter, gpointer win){
+static void background_dialog(GSimpleAction * action, GVariant * parmeter, MainWin *win){
 }
 
-static GActionEntry entries = {
-    
+static void default_background1(GSimpleAction * action, GVariant * parmeter, MainWin *win){}
+
+static void default_background2(GSimpleAction * action, GVariant * parmeter, MainWin *win){}
+
+static void gesture_pressed(GtkGestureClick *self,int n_press,double x,double y,MainWin *win){
+    GdkRectangle rect;
+    rect.x=x;
+    rect.y=y;
+    rect.height=1;
+    rect.width=1;
+    gtk_popover_set_pointing_to(GTK_POPOVER(win->popover),&rect);
+    gtk_popover_popup(GTK_POPOVER(win->popover));
+}
+
+static GActionEntry entries[] = {
+    {"back",background_dialog,NULL,NULL,NULL}
 };
 
 static void main_win_init(MainWin * win){
@@ -31,14 +46,28 @@ static void main_win_init(MainWin * win){
     gtk_window_set_default_size(GTK_WINDOW(win),win->width,win->height);
     gtk_window_set_title(GTK_WINDOW(win),"My GtkUI (Gtk4 Version)");
 
+    //Add Actions
+    g_action_map_add_action_entries(G_ACTION_MAP(win),entries,G_N_ELEMENTS(entries),win);
+
     //Add Overlay and background widget
     win->overlay = gtk_overlay_new();
-    win->draw_area = gtk_drawing_area_new();
     win->background = gtk_picture_new();
     gtk_overlay_set_child(GTK_OVERLAY(win->overlay),win->background);
-    gtk_widget_set_halign(win->draw_area,GTK_ALIGN_FILL);
-    gtk_widget_set_valign(win->draw_area,GTK_ALIGN_FILL);
-    gtk_overlay_add_overlay(GTK_OVERLAY(win->overlay),win->draw_area);
+
+    //Add Menu
+    GtkBuilder * menu_builder = gtk_builder_new_from_resource("/org/gtk/daleclack/appmenu.xml");
+    GMenuModel * model = G_MENU_MODEL(gtk_builder_get_object(menu_builder,"app-menu"));
+    win->popover = gtk_popover_menu_new_from_model(model);
+    gtk_popover_set_has_arrow(GTK_POPOVER(win->popover),FALSE);
+    gtk_widget_set_parent(win->popover,win->overlay);
+
+    //Add Gesture to show menu
+    win->gesture = gtk_gesture_click_new();
+    gtk_gesture_single_set_button(GTK_GESTURE_SINGLE(win->gesture),GDK_BUTTON_SECONDARY);
+    g_signal_connect(win->gesture,"pressed",G_CALLBACK(gesture_pressed),win);
+    gtk_widget_add_controller(win->overlay,GTK_EVENT_CONTROLLER(win->gesture));
+
+    gtk_window_set_child(GTK_WINDOW(win),win->overlay);
 }
 
 static void main_win_class_init(MainWinClass * klass){}
