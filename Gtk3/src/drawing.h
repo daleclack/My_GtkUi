283bee42aa50caeeaf2e030d4f7dcb17bbe89d6e
daleclack <daleclack1@foxmail.com>
2021-12-29 22:34:39 +0800

    Delete Unused files

--- Gtk3/src/drawing.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DRAWING_H_
-#define __DRAWING_H_
-
-#include <gtk/gtk.h>
-
-void drawing_main(GtkWidget *widget,GtkWindow *parent);
-
-#endif
--- Gtk3/src/win1.h
+++
@@ -1,8 +0,0 @@
-#ifndef __WIN1_H_
-#define __WIN1_H_
-
-#include <gtk/gtk.h>
-
-void win1_init(GtkWidget *widget,GtkWindow *parent);
-
-#endif
