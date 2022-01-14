36d43e601d4591fd5abf33a2036d6b63844f7a66
daleclack <daleclack1@foxmail.com>
2022-01-14 22:04:00 +0800

    Update start menu

--- Flos_Gtk4/src/core/Start.cpp
+++ Flos_Gtk4/src/core/Start.cpp
@@ -8,6 +8,14 @@ static void btnback_clicked(GtkWidget * widget, GtkStack * stack){
     gtk_stack_set_visible_child_name(stack,"page0");
 }
 
+static void btnmusic_clicked(GtkWidget * widget, GtkWidget * music_box){
+    if(gtk_widget_get_visible(music_box)){
+        gtk_widget_set_visible(music_box,FALSE);
+    }else{
+        gtk_widget_set_visible(music_box,TRUE);
+    }
+}
+
 void add_start(MainWin * win, GtkBox * box){
     //Create a builder for menu
     GtkBuilder * startbuilder = gtk_builder_new_from_resource("/org/gtk/daleclack/start.ui");
@@ -36,6 +44,16 @@ void add_start(MainWin * win, GtkBox * box){
                                    main_win_get_style(win), G_MAXINT);
     gtk_style_context_add_provider(gtk_widget_get_style_context(ext_separator),
                                    main_win_get_style(win), G_MAXINT);
+
+    //Integrated Media Player
+    GtkWidget * music_box = (GtkWidget*)gtk_builder_get_object(startbuilder,"music_box");
+    GtkWidget * btn_music = (GtkWidget*)gtk_builder_get_object(startbuilder,"btnmusic");
+    GtkWidget * media_control = (GtkWidget*)gtk_builder_get_object(startbuilder,"media_ctrl");
+    g_signal_connect(btn_music,"clicked",G_CALLBACK(btnmusic_clicked),music_box);
+
+    //Add music resource
+    GtkMediaStream * media = gtk_media_file_new_for_resource("/org/gtk/daleclack/Yanni-Nightingale.mp3");
+    gtk_media_controls_set_media_stream(GTK_MEDIA_CONTROLS(media_control),media);
 
     //Pack widgets
     gtk_box_prepend(box,menu_btn);
