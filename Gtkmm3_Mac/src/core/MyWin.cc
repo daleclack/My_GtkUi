bf9370f7cbfbe1f25a8c541028f87546834677ed
daleclack <daleclack1@foxmail.com>
2022-01-22 18:16:24 +0800

    Change Menu to buttons

--- Gtkmm3_Mac/src/core/MyWin.cc
+++ Gtkmm3_Mac/src/core/MyWin.cc
@@ -1,7 +1,9 @@
 #include "MyWin.hh"
 #include "winpe.xpm"
 
-MyWin::MyWin(){
+MyWin::MyWin()
+:menubox(Gtk::ORIENTATION_HORIZONTAL,0)
+{
     //Initalize window
     set_default_size(1024,576);
     set_icon_name("My_GtkUI");
@@ -15,14 +17,67 @@ MyWin::MyWin(){
 
     //Add a menubar
     auto menubuilder = Gtk::Builder::create_from_resource("/org/gtk/daleclack/menubar.xml");
-    auto object = menubuilder->get_object("model");
-    auto gmenu = Glib::RefPtr<Gio::Menu>::cast_dynamic(object);
 
-    menubar = Gtk::make_managed<Gtk::MenuBar>(gmenu);
-    menubar->set_halign(Gtk::ALIGN_FILL);
-    menubar->set_valign(Gtk::ALIGN_START);
-    m_overlay.add_overlay(*menubar);
+    //Add widgets to menubox
+    btnlogo.set_image_from_icon_name("My_GtkUI",Gtk::ICON_SIZE_BUTTON);
+    btnlogo.set_relief(Gtk::RELIEF_NONE);
+    btnlogo.set_use_popover(false);
+    btnlogo.set_menu_model(get_menu(menubuilder,"main_menu"));
+    menubox.pack_start(btnlogo,Gtk::PACK_SHRINK);
+
+    btntitle.set_label("MyFinder");
+    btntitle.set_relief(Gtk::RELIEF_NONE);
+    btntitle.set_use_popover(false);
+    btntitle.set_menu_model(get_menu(menubuilder,"title_menu"));
+    menubox.pack_start(btntitle,Gtk::PACK_SHRINK);
+
+    btnfile.set_label("File");
+    btnfile.set_relief(Gtk::RELIEF_NONE);
+    btnfile.set_use_popover(false);
+    btnfile.set_menu_model(get_menu(menubuilder,"menu_file"));
+    menubox.pack_start(btnfile,Gtk::PACK_SHRINK);
+
+    btnedit.set_label("Edit");
+    btnedit.set_relief(Gtk::RELIEF_NONE);
+    btnedit.set_use_popover(false);
+    btnedit.set_menu_model(get_menu(menubuilder,"menu_edit"));
+    menubox.pack_start(btnedit,Gtk::PACK_SHRINK);
+
+    btnshow.set_label("Show");
+    btnshow.set_relief(Gtk::RELIEF_NONE);
+    btnshow.set_use_popover(false);
+    btnshow.set_menu_model(get_menu(menubuilder,"menu_show"));
+    menubox.pack_start(btnshow,Gtk::PACK_SHRINK);
+
+    btngoto.set_label("Go to");
+    btngoto.set_relief(Gtk::RELIEF_NONE);
+    btngoto.set_use_popover(false);
+    btngoto.set_menu_model(get_menu(menubuilder,"menu_goto"));
+    menubox.pack_start(btngoto,Gtk::PACK_SHRINK);
+
+    btnwin.set_label("Windows");
+    btnwin.set_relief(Gtk::RELIEF_NONE);
+    btnwin.set_use_popover(false);
+    btnwin.set_menu_model(get_menu(menubuilder,"menu_win"));
+    menubox.pack_start(btnwin,Gtk::PACK_SHRINK);
+
+    btnhelp.set_label("Help");
+    btnhelp.set_relief(Gtk::RELIEF_NONE);
+    btnhelp.set_use_popover(false);
+    btnhelp.set_menu_model(get_menu(menubuilder,"menu_help"));
+    menubox.pack_start(btnhelp,Gtk::PACK_SHRINK);
+
+    //Add menu to window
+    menubox.set_halign(Gtk::ALIGN_FILL);
+    menubox.set_valign(Gtk::ALIGN_START);
+    m_overlay.add_overlay(menubox);
 
     add(m_overlay);
     show_all_children();
+}
+
+Glib::RefPtr<Gio::Menu> MyWin::get_menu(Glib::RefPtr<Gtk::Builder> &builder, const Glib::ustring &id){
+    auto object = builder->get_object(id);
+    auto gmenu = Glib::RefPtr<Gio::Menu>::cast_dynamic(object);
+    return gmenu;
 }
--- Gtkmm3_Mac/src/core/MyWin.hh
+++ Gtkmm3_Mac/src/core/MyWin.hh
@@ -9,5 +9,10 @@ private:
     //Child widgets
     Gtk::Overlay m_overlay;
     Gtk::Image m_background;
-    Gtk::MenuBar * menubar;
+    Gtk::Box menubox;
+    Gtk::MenuButton btnlogo, btntitle, btnfile, btnedit, btnshow, 
+                    btngoto, btnwin, btnhelp;
+
+    //Get Menu
+    Glib::RefPtr<Gio::Menu> get_menu(Glib::RefPtr<Gtk::Builder> &builder, const Glib::ustring &id);
 };
