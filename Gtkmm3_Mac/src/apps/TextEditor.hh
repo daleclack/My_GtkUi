0533c96cfe448309a056933c33967ee2eb6fed92
daleclack <daleclack1@foxmail.com>
2022-07-08 13:46:12 +0800

    Update text editor

--- Gtkmm3_Mac/src/apps/TextEditor.hh
+++ Gtkmm3_Mac/src/apps/TextEditor.hh
@@ -2,29 +2,57 @@
 
 #include <gtkmm.h>
 
-class TextEditor : public Gtk::Window{
+class TextEditor : public Gtk::ApplicationWindow{
 public:
     TextEditor();
 private:
-    //Child widgets
-    Gtk::Box vbox,hbox,btnbox,*infobox;
+    //Header widgets
+    Gtk::HeaderBar header;
+    Gtk::MenuButton menubtn;
+    Gtk::Popover popover;
+    Gtk::ToggleButton search_button;
+    Glib::RefPtr<Gtk::Builder> menu_builder;
+
+    //SearchBar
+    Gtk::SearchBar searchbar;
+    Gtk::SearchEntry search_entry;
+    Gtk::Box searchbox;
+    Gtk::Button search_up, search_down;
+    Glib::RefPtr<Glib::Binding> search_binding;
+    Gtk::TextIter curr_iter_up, curr_iter_down;
+
+    //Window widgets
+    Gtk::Box vbox,hbox,*infobox;
     Gtk::ScrolledWindow sw1,sw2;
     Glib::RefPtr<Gtk::TextBuffer> buffer1;
     Gtk::TextView textview1;
-    Gtk::Button btn_copy,btn_paste,btn_open,btn_save,btn_clear;
     Gtk::InfoBar infobar;
     Gtk::Label label1;
+
     //File Dialog
     Glib::RefPtr<Gtk::FileChooserNative> dialog;
+    Glib::ustring curr_filename;
+    bool file_opened;
+
     //Signal Handlers
+
+    // File Operation functions
     void btnopen_clicked();
     void opendialog_response(int response);
     void btnsave_clicked();
+    void btnsaveas_clicked();
     void savedialog_response(int response);
+
+    // Copy, Paste and text operations
     void btncopy_clicked();
     void btnpaste_clicked();
-    void btnclear_clicked();
+    void btnclose_clicked();
     void buffer1_changed();
     void clipboard_receive(const Glib::ustring &text);
     void infobar_response(int response);
+
+    // Search funtion
+    void search_entry_changed();
+    void search_forward();
+    void search_backward();
 };
